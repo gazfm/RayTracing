bc1ebc5aee33b84d99da7d4fbab1873b64b86aaf
Garry Ferguson <Garry Ferguson>
2016-09-15 20:26:00 +0100

    Broken attempt at specular highlight version

--- raytracer_cpp/main.cpp
+++ raytracer_cpp/main.cpp
@@ -41,7 +41,7 @@ void InitScene()
     mat.reflectance = 0.0f;
     mat.emissive = vec3(0.0f, 0.0f, 0.0f);
     sceneObjects.push_back(std::make_shared<Sphere>(mat, vec3(-0.0f, 0.5f, 3.f), 0.5f));
-    
+
     // Yellow ball on floor
     mat.albedo = vec3(1.0f, 1.0f, 1.0f);
     mat.specular = vec3(0.0f, 0.0f, 0.0f);
@@ -60,14 +60,70 @@ void InitScene()
     sceneObjects.push_back(std::make_shared<TiledPlane>(vec3(0.0f, 0.0f, 0.0f), normalize(vec3(0.0f, 1.0f, 0.0f))));
 }
 
+std::shared_ptr<SceneObject> FindFirstIntersector(const vec3 from, const vec3 dir, float& dst)
+{
+    float minDistance = FLT_MAX;
+    std::shared_ptr<SceneObject> pObject;
+
+    for (const auto& obj : sceneObjects)
+    {
+        float distance;
+        if (obj->Intersects(from, dir, distance))
+        {
+            if (distance < minDistance)
+            {
+                pObject = obj;
+                minDistance = distance;
+            }
+
+        }
+    }
+
+    dst = minDistance;
+    return pObject;
+}
+
 // Trace a ray into the scene, return the accumulated light value
 vec3 TraceRay(const vec3& rayorig, const vec3 &raydir, const int depth)
 {
     // Here's how to print out a vector for inspection
     //std::cout << "Ray: " << glm::to_string(raydir) << std::endl;
 
+
+    auto minDistance = 0.0f;
+    auto obj = FindFirstIntersector(rayorig, raydir, minDistance);
+
+    auto interPos = rayorig + glm::normalize(raydir) * minDistance;
+    auto normal = obj->GetSurfaceNormal(interPos);
+    auto albedo = obj->GetMaterial(interPos).albedo;
+
+    auto color = obj->GetMaterial(vec3(0, 0, 0)).emissive;
+
+    //color = vec3(0, 0, 0);
+    for (const auto& light : sceneObjects)
+    {
+        vec3 rayFrom = light->GetRayFrom(interPos);
+        rayFrom = glm::normalize(rayFrom);
+
+        float dis;
+        auto occluder = FindFirstIntersector(interPos + rayFrom * 0.001f, rayFrom, dis);
+        auto occlusionOccurred = occluder != light;
+
+        if (!occlusionOccurred)
+        {
+            auto intensity = std::max(glm::dot(normal, rayFrom), 0.0f);
+            color += albedo * light->GetMaterial(vec3(0, 0, 0)).emissive * intensity;
+
+            auto reflection = glm::reflect(raydir, normal);
+            auto specular_intensity = glm::dot(reflection, rayFrom);
+            specular_intensity = specular_intensity * specular_intensity;
+            color += obj->GetMaterial(vec3(0, 0, 0)).specular * specular_intensity;
+        }
+    }
+
+
     // For now, just convert the incoming ray to a 'color' to display it has it changes 
-    return vec3((raydir * .5f) + vec3(0.5f, 0.5f, 0.5f));
+    return color;
 }
 
 // Draw the scene by generating camera rays and tracing rays
